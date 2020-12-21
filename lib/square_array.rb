b61a4c27080e034c0fb0e7c25e81d093f02a8e33
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-21 18:27:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,15 +1,10 @@
 def square_array(array)
   squared_nums = []
   
-  array.length.times do |num|
-    squared_nums << (num * num)
+  array.length.times do |idx|
+    squared_nums << (array[idx] * array[idx])
   end
     
   return squared_nums
 end
 
-array = [1,2,3,4,5]
- 
-array.length.times { |index|
-  puts array[index]
-}
